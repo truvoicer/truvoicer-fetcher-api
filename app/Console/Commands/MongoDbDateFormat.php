e2fa3e4fefdf0980b96ebffd353455381227269e
bigriddler97 <mikydxl@gmail.com>
2024-12-24 00:33:19 +0000

    importer

--- app/Console/Commands/MongoDbDateFormat.php
+++ app/Console/Commands/MongoDbDateFormat.php
@@ -2,6 +2,7 @@
 
 namespace App\Console\Commands;
 
+use App\Helpers\Tools\DateHelpers;
 use App\Http\Requests\Admin\User\CreateUserRequest;
 use App\Models\Role;
 use App\Helpers\Tools\UtilHelpers;
@@ -15,11 +16,14 @@ use Carbon\Carbon;
 use Illuminate\Console\Command;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Support\Facades\Validator;
+use MongoDB\BSON\UTCDateTime;
 use Symfony\Component\Console\Command\Command as CommandAlias;
 
 class MongoDbDateFormat extends Command
 {
     private SrResponseKeyService $srResponseKeyService;
+    private SrRepository $srRepository;
+    private MongoDBRepository $mongoDBRepository;
     /**
      * The name and signature of the console command.
      *
@@ -45,61 +49,93 @@ class MongoDbDateFormat extends Command
         SrRepository $srRepository
     )
     {
+        $this->mongoDBRepository = $mongoDBRepository;
         $this->srResponseKeyService = $srResponseKeyService;
-
-        $srs = $srRepository->findAll();
-        foreach ($srs as $sr) {
-            if (!$sr->provider()->where('name', 'reed')->exists()) {
-                continue;
+        $this->srRepository = $srRepository;
+        $srs = $this->srRepository->findAll();
+        $this->collectionIterator(
+            $srs,
+            function () {
+                return [
+                    ['key' =>'updated_at', 'format' => null],
+                    ['key' =>'created_at', 'format' => null],
+                ];
             }
-            $collectionName = $mongoDBRepository->getCollectionName($sr);
+        );
+        $this->collectionIterator(
+            $srs,
+            function (Sr $sr) {
+                $dateKeys = $this->getDateResponseKeys($sr);
+                if (!empty($defaultData['date']) && !$dateKeys->where('name', 'date_published')->first()) {
+                    $srResponseKey = $this->srResponseKeyService->getSrResponseKeyRepository()->findOneSrResponseKeysWithRelation(
+                        $sr,
+                        [],
+                        ['name' => 'date_published']
+                    );
+                    if ($srResponseKey) {
+                        $dateKeys->add($srResponseKey);
+                    }
+                }
+                $data = [];
+                foreach ($dateKeys as $dateKey) {
+                    $data[] = ['key' => $dateKey->name, 'format' => $dateKey?->srResponseKey?->date_format];
+                }
+                return $data;
+            }
+        );
+        return CommandAlias::SUCCESS;
+    }
+
+    private function collectionIterator(Collection $srs, \Closure $callback) {
+
+        foreach ($srs as $sr) {
+            $collectionName = $this->mongoDBRepository->getCollectionName($sr);
             if (!$collectionName) {
                 $this->error('Invalid collection name');
                 return CommandAlias::FAILURE;
             }
-            $mongoDBRepository->setCollection($collectionName);
+            $this->mongoDBRepository->setCollection($collectionName);
 
             $defaultData = $sr->default_data;
-            $dateKeys = $this->getDateResponseKeys($sr);
-
-            if (!empty($defaultData['date']) && !$dateKeys->where('name', 'date_published')->first()) {
-                $srResponseKey = $this->srResponseKeyService->getSrResponseKeyRepository()->findOneSrResponseKeysWithRelation(
-                    $sr,
-                    [],
-                    ['name' => 'date_published']
-                );
-                if ($srResponseKey) {
-                    $dateKeys->add($srResponseKey);
-                }
-            }
-
-            foreach ($mongoDBRepository->findAll() as $document) {
+            $dateKeys = $callback($sr);
+            foreach ($this->mongoDBRepository->findAll() as $document) {
+                $updateData = [];
                 foreach ($dateKeys as $dateKey) {
-                    if (empty($document[$dateKey->name])) {
+                    if (empty($document[$dateKey['key']])) {
                         continue;
                     }
-                    $date = $document[$dateKey->name];
-                    $isDate = $dateKey->srResponseKey?->is_date;
-                    $dateFormat = $dateKey->srResponseKey?->date_format;
-
-                    $newDate = null;
-                    try {
-                        if ($dateFormat) {
-                            $newDate = Carbon::createFromFormat($dateFormat, $date)->toISOString();
-                        } else {
-                            $newDate = Carbon::parse($date)->toISOString();
+                    $date = $document[$dateKey['key']];
+                    $dateFormat = $dateKey['format'];
+                    if ($date instanceof UTCDateTime) {
+                        $newDate = Carbon::createFromTimestamp($date);
+                        if (!$newDate->greaterThan(Carbon::now())) {
+                            continue;
                         }
-                    } catch (\Exception $e) {
-                        if (str_contains($date, '/')) {
-                            $newDate = Carbon::parse(str_replace('/', '-', $date))->toISOString();
+                        $newDate = $document['updated_at'];
+                        $updateData[$dateKey['key']] = $newDate;
+                    } else {
+                        $newDate = DateHelpers::parseDateString($date, $dateFormat);
+                        if (!$newDate) {
+                            $type = gettype($date);
+                            $this->error("Error parsing date: {$date} | type: {$type} | collection: {$collectionName} | dateKey: {$dateKey['key']}");
+                            continue;
                         }
-                        dd($dateFormat, $date, $newDate);
+                        $updateData[$dateKey['key']] = new UTCDateTime($newDate);
                     }
                 }
-//                $mongoDBRepository->update($document);
+
+                if (count($updateData) && !$this->mongoDBRepository->update($document['_id'], $updateData)) {
+                    $this->error(
+                        sprintf(
+                            "Error updating document collection: %s | id: %s | data: %s",
+                            $collectionName,
+                            $document['_id'],
+                            json_encode($updateData, JSON_PRETTY_PRINT)
+                        )
+                    );
+                }
             }
         }
-        return CommandAlias::SUCCESS;
     }
     private function getDateResponseKeys(Sr $sr): Collection
     {
